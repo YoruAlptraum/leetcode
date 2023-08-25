280ed3273331bb83f9e2d1240e2c8877ba7b9a17
Thiago Daisuke Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2023-08-25 18:02:25 -0300

    Create 2. Add Two Numbers.py
    Added problem 2. the answer was accepted on leetcode but the answer is not displaying properly on the result. need to check the end of the code

---
+++ python/2. Add Two Numbers.py
@@ -0,0 +1,42 @@
+from typing import Optional, List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def createList(arr: List[int]) -> Optional[ListNode]:
+    prev = None
+    head = None
+    for i in range(len(arr)):
+        if i == 0:
+            head = prev = ListNode(arr[0])
+        else:
+            prev = ListNode(arr[i], prev)
+    return head
+
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    carry = 0
+    head = ListNode()
+    current = head
+    while l1 or l2 or carry:
+        sum = carry
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+
+        carry = sum // 10
+        current.next = ListNode(sum % 10)
+        current = current.next
+    return head.next
+
+l1 = createList([2,4,3]) # 342
+l2 = createList([5,6,4]) # 465
+l3 = addTwoNumbers(l1, l2) # expected 807
+
+while l3:
+    print(l3.val)
+    l3 = l3.next
