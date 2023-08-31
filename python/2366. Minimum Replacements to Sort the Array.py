52b6f9a6c7cf696417423b73588b450f615451de
Thiago Daisuke Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2023-08-31 17:52:45 -0300

    updated code

--- python/2366. Minimum Replacements to Sort the Array.py
+++ python/2366. Minimum Replacements to Sort the Array.py
@@ -7,17 +7,23 @@ def minimumReplacement(nums: List[int]) -> int:
     rep = 0
     prev = nums[lnums-1]
     for i in range(lnums-2,-1,-1):
-        t = nums[i]/prev
         if nums[i] > prev:
-            rep += t
-        for i in t:
-            nums[i] -= nums[i]%prev
+            t = nums[i]//prev
+            rep += t -1
+            if nums[i] % prev != 0:
+                rep += 1
+            
+            if nums[i] // 2 <= prev:
+                nums[i] = nums[i] // 2
+            else:
+                nums[i] = nums[i] // t
         prev = nums[i]
     return rep
 
-print('expected 6 |', minimumReplacement([12,9,7,6,17,19,21]))
-print('expected 2 |', minimumReplacement([3,9,3]))
-print('expected 2 |', minimumReplacement([9,3]))
-print('expected 2 |', minimumReplacement([9,4]))
-# print('expected 0 |', minimumReplacement([1,2,3,4,5]))
-# print('expected 0 |', minimumReplacement([1]))
+print('expected 20 |', minimumReplacement([1,13,15,2,5,14,12,17]))
+print('expected  6 |', minimumReplacement([12,9,7,6]))
+print('expected  2 |', minimumReplacement([3,9,3]))
+print('expected  2 |', minimumReplacement([9,3]))
+print('expected  2 |', minimumReplacement([9,4]))
+print('expected  0 |', minimumReplacement([1,2,3,4,5]))
+print('expected  0 |', minimumReplacement([1]))
