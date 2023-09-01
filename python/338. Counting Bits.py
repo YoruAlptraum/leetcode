f3638ad7f03bd70e22d34068e640ec2718ea4d67
Thiago Daisuke Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2023-09-01 17:56:52 -0300

    Create 338. Counting Bits.py
    added 338 using conversion.
    Need to study more to try a version using bit operations

---
+++ python/338. Counting Bits.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def countBits(n: int) -> List[int]:
+    ans = []
+    for i in range(n + 1):
+        sum = 0
+        s = str(bin(i))
+        print(s)
+        for j in range(2,len(s)):
+            sum += int(s[j])
+        ans.append(sum)
+    return ans
+
+print(countBits(10))
+print([0,1,1,2,1,2,2,3,1,2,2])
