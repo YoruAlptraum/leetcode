98d14f143eaf744b56f28be17987ac9a36ef9d4e
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2023-09-07 21:47:57 -0300

    Update 725. Split Linked List in Parts.py
    Finished the problem, took quite a bit more effort than initially expected and required a few hints but manage to complete it

--- python/725. Split Linked List in Parts.py
+++ python/725. Split Linked List in Parts.py
@@ -6,24 +6,44 @@ class ListNode:
         self.val = val
         self.next = next
 
+def makeList(arr: List[Optional[int]]):
+    head = ListNode()
+    dummy = head
+    for i in arr:
+        head.next = ListNode(i)
+        head = head.next
+    return dummy.next
+            
+
 def splitListToParts(head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-    heads = [head] * k
     n = 0
-    while head:
+    cur = head
+    while cur:
         n+=1
-        head = head.next
-    limit = n//k
-    head = heads[0]
-    i = 0
-    curhead = 1
-    while head:
-        i+=1
-        if i == limit:
-            heads[curhead % k] = head
-            curhead+=1
-            i = 0
-        head = head.next
+        cur = cur.next
+    extra = n%k
+
+    heads = [head]
+    
+    for i in range(k-1):
+        j = 0
+        while head and (j < n//k-1 or (i<extra and j < n//k)):
+            j+=1
+            head = head.next
+        if head:
+            head.next, head = None, head.next
+        heads.append(head)
     return heads
 
 if __name__ == "__main__":
-    splitListToParts()
+    head = makeList([1,2,3,4,5,6,7,8,9,10]) # expected [[1,2,3,4],[5,6,7],[8,9,10]]
+    ans = splitListToParts(head, k = 3)
+    final = []
+    for i in ans:
+        arr = []
+        while i:
+            arr.append(i.val)
+            i = i.next
+        final.append(arr)
+    print(final)
+    print([[1,2,3,4],[5,6,7],[8,9,10]])
