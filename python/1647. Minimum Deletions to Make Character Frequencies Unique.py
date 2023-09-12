710c78bbacd4f643143a3babb5eb33e71fee69aa
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2023-09-12 15:55:05 -0300

    Update 1647. Minimum Deletions to Make Character Frequencies Unique.py
    updated 1647 to be faster, it was previously 194ms on leetcode now it is 108ms

--- python/1647. Minimum Deletions to Make Character Frequencies Unique.py
+++ python/1647. Minimum Deletions to Make Character Frequencies Unique.py
@@ -1,21 +1,14 @@
+from collections import Counter
 
 def minDeletions(s: str) -> int:
+    chars = Counter(s)
     d = 0
-    freq = {}
-    # count freq of each char
-    i = 0
-    for i in s:
-        if i in freq:
-            freq[i] += 1            
-        else:
-            freq[i] = 1
     lis = []
-    for i in freq.values():
+    for i in chars.values():
         while i in lis and i != 0:
             i -= 1
             d += 1
-        if i not in lis:
-            lis.append(i)
+        lis.append(i)
     return d
 
 if __name__ == "__main__":
