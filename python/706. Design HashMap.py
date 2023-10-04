488e14cef730b5fe4e9c994471a0cac4a649e30d
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2023-10-03 22:50:16 -0300

    Added 706

---
+++ python/706. Design HashMap.py
@@ -0,0 +1,53 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.ks = []
+        self.vs = []
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.ks:
+            self.vs[self.ks.index(key)] = value
+        else:
+            self.ks.append(key)
+            self.vs.append(value)
+
+    def get(self, key: int) -> int:
+        if key in self.ks:
+            return self.vs[self.ks.index(key)]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.ks:
+            ind = self.ks.index(key)
+            self.ks.pop(ind)
+            self.vs.pop(ind)
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
+if __name__ == "__main__":
+    cases = [
+        [
+            ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"],
+            [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+        ]
+    ]
+    ans = []
+    for c in cases:
+        for i in range(len(c[0])): 
+            method_name = c[0][i]
+            args = c[1][i]
+            if method_name == "MyHashMap":
+                obj = MyHashMap()
+                ans.append(None)
+            elif method_name == "put":
+                ans.append(obj.put(*args))
+            elif method_name == "get":
+                ans.append(obj.get(*args))
+            elif method_name == "remove":
+                ans.append(obj.remove(*args))
+    print('passed' if ans == [None, None, None, 1, -1, None, 1, None, -1] else 'failed')
